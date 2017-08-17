ea66b68b9f23f200792f56a6bd8fd23d54ef5ee7
yogolas <yogolas@gmail.com>
2017-08-17 16:06:20 +0700

    master c2mdoc

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,31 @@
+<?php
+ class MY_Controller extends CI_Controller {
+ 
+
+ 	 public function __construct()
+    {
+        parent::__construct();
+         $this->load->library('session');
+        
+        $this->base_url = 'http://localhost.com/c2mdoc';
+
+    }
+
+ public function mainlayout($view,$data) {
+ // Page local resource 
+$data['base_url'] = $this->base_url;
+
+$this->load->view('layout/main/header.php',$data);
+ $this->load->view('layout/main/left.php',$data);
+  $this->load->view($view,$data);
+ $this->load->view('layout/main/right.php',$data);
+ $this->load->view('layout/main/footer.php',$data);
+ }
+
+  
+
+
+
+
+
+ }
---
+++ application/views/layout/main/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Latest compiled and minified CSS & JS -->
+<link rel="stylesheet" href="<?php echo $base_url; ?>/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+<script src="<?php echo $base_url; ?>/js/jquery.min.js"></script>
+<script src="<?php echo $base_url; ?>/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script src="<?php echo $base_url; ?>/js/angular.min.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/morris.css">
+<script src="<?php echo $base_url; ?>/js/raphael-min.js"></script>
+<script src="<?php echo $base_url; ?>/js/morris.min.js"></script>
+
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/toastr.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/css.css">
+<script src="<?php echo $base_url; ?>/js/toastr.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89792612-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"> 
+
+
+<style type="text/css">
+	body{
+		font-family: 'Kanit', sans-serif;
+	}
+	
+</style>
+
+
+
+
+	<title><?php echo $title; ?></title>
+</head>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo $base_url; ?>/" style="color: #000;"><b>Google Map API</b></a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			
+			
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="<?php echo $base_url; ?>/login" style="color: #000;">เข้าสู่ระบบ</a></li>
+				
+				
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+
+
